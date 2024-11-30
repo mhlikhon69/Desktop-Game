83502fd538005b1a18f1663520b0d5c8bd0dc7de
mhlikhon69 <mhlikhon2003@gmail.com>
2024-11-30 23:04:00 +0600

    add pipe

---
+++ src/FlappyBird.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class FlappyBird extends JPanel implements ActionListener, KeyListener {
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    //images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+    //bird class
+    int birdX = boardWidth/8;
+    int birdY = boardWidth/2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+    class Bird {
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img) {
+            this.img = img;
+        }
+    }
+
+}
