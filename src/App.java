1fe5a8c87db597913686342eac1ce2c9f7c69dae
mhlikhon69 <mhlikhon2003@gmail.com>
2024-12-01 22:46:48 +0600

    First Commit

--- src/App.java
+++ src/App.java
@@ -2,11 +2,11 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int boardWidth = 360;
+        int boardWidth = 960;
         int boardHeight = 640;
 
         JFrame frame = new JFrame("Flappy Bird");
-        // frame.setVisible(true);
+        //frame.setVisible(true);
 		frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,35 +1,44 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
-import java.util.Random;
 import javax.swing.*;
 
-public class FlappyBird extends JPanel implements ActionListener, KeyListener {
-    int boardWidth = 360;
+
+
+public class FlappyBird extends JPanel {
+    int boardwidth = 960;
     int boardHeight = 640;
 
-    //images
+
+    //image
     Image backgroundImg;
     Image birdImg;
-    Image topPipeImg;
-    Image bottomPipeImg;
+    Image toppipeImg;
+    Image bottompipeImg;
 
-    //bird class
-    int birdX = boardWidth/8;
-    int birdY = boardWidth/2;
-    int birdWidth = 34;
-    int birdHeight = 24;
 
-    class Bird {
-        int x = birdX;
-        int y = birdY;
-        int width = birdWidth;
-        int height = birdHeight;
-        Image img;
+    FlappyBird(){
+        setPreferredSize(new Dimension(boardwidth, boardHeight));
+        //setBackground(Color.red);
 
-        Bird(Image img) {
-            this.img = img;
-        }
+
+
+        //load image
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+
+        toppipeImg = new ImageIcon (getClass().getResource("./toppipe.png")).getImage();
+
+        bottompipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
     }
 
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw (g);
+    }
+
+
+    public void draw(Graphics g){
+        
+    }
 }
